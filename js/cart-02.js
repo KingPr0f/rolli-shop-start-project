a78ea62063ef52bfea051683cbd641b1e4931ae2
KingPr0f <strange98752@gmail.com>
2024-06-14 13:15:44 +1000

    newUpdate1

--- js/cart-02.js
+++ js/cart-02.js
@@ -1,27 +1,40 @@
-const cartWrapper = document.querySelector('.cart-wrapper');
-window.addEventListener('click', (event) => {
-    if (event.target.hasAttribute('data-cart')) {
-        const card = event.target.closest('.card');
-        
-        const productInfo = {
-            id: card.dataset.id,
-            imgSrc: card.querySelector('.product-img').getAttribute('src'),
-            title: card.querySelector('.item-title').innerText,
-            itemsInBox: card.querySelector('[data-items-in-box]').innerText,
-            counter: card.querySelector('[data-counter]').innerText,
-            weight: card.querySelector('.price__weight').innerText,
-            price: card.querySelector('.price__currency').innerText
-        }
-        const itemInCard = cartWrapper.querySelector(`[data-id = "${productInfo.id}"]`);
-        if(itemInCard) {
-            const itemCounter = itemInCard.querySelector('[data-counter]');
-            itemCounter.innerHTML = parseInt(itemCounter.innerHTML) + parseInt(productInfo.counter);
+// Div внутри корзины, в который мы добавляем товары
+const cartWrapper =  document.querySelector('.cart-wrapper');
 
-        } else {
-        const cartItemHTML = `<div class="cart-item" data-id="${productInfo.id}">
+// Отслеживаем клик на странице
+window.addEventListener('click', function (event) {
+	// Проверяем что клик был совершен по кнопке "Добавить в корзину"
+	if (event.target.hasAttribute('data-cart')) {
+
+		// Находим карточку с товаром, внутри котрой был совершен клик
+		const card = event.target.closest('.card');
+
+		// Собираем данные с этого товара и записываем их в единый объект productInfo
+		const productInfo = {
+			id: card.dataset.id,
+			imgSrc: card.querySelector('.product-img').getAttribute('src'),
+			title: card.querySelector('.item-title').innerText,
+			itemsInBox: card.querySelector('[data-items-in-box]').innerText,
+			weight: card.querySelector('.price__weight').innerText,
+			price: card.querySelector('.price__currency').innerText,
+			counter: card.querySelector('[data-counter]').innerText,
+		};
+
+		// Проверять если ли уже такой товар в корзине
+		const itemInCart = cartWrapper.querySelector(`[data-id="${productInfo.id}"]`);
+
+		// Если товар есть в корзине
+		if (itemInCart) {
+			const counterElement = itemInCart.querySelector('[data-counter]');
+			counterElement.innerText = parseInt(counterElement.innerText) + parseInt(productInfo.counter);
+		} else {
+			// Если товара нет в корзине
+
+			// Собранные данные подставим в шаблон для товара в корзине
+			const cartItemHTML = `<div class="cart-item" data-id="${productInfo.id}">
 								<div class="cart-item__top">
 									<div class="cart-item__img">
-										<img src="${productInfo.imgSrc}" alt="">
+										<img src="${productInfo.imgSrc}" alt="${productInfo.title}">
 									</div>
 									<div class="cart-item__desc">
 										<div class="cart-item__title">${productInfo.title}</div>
@@ -45,9 +58,20 @@ window.addEventListener('click', (event) => {
 
 									</div>
 								</div>
-							</div>`
-        cartWrapper.insertAdjacentHTML('beforeend', cartItemHTML );
-        }
-        card.querySelector('[data-counter]').innerText = '1';
-    }
-})
+							</div>`;
+
+			// Отобразим товар в корзине
+			cartWrapper.insertAdjacentHTML('beforeend', cartItemHTML);
+		}
+
+		// Сбрасываем счетчик добавленного товара на "1"
+		card.querySelector('[data-counter]').innerText = '1';
+
+		// Отображение статуса корзины Пустая / Полная
+		toggleCartStatus();
+
+		// Пересчет общей стоимости товаров в корзине
+		calcCartPriceAndDelivery();
+
+	}
+});
